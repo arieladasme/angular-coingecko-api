dfb241ea8dc1b401089e3a1236ded7c3c503fe6c
ariel <ariel.wm1@gmail.com>
2023-05-05 12:24:08 -0400

    filter coins

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,23 +18,34 @@ interface Coin {
 })
 export class AppComponent implements OnInit {
   coins: Coin[] = [];
+  filteredCoins: Coin[] = [];
   titles: string[] = ['#', 'Coin', 'Price', 'Price Change', '24h Volume'];
+  searchText: string = '';
 
   constructor(private http: HttpClient) {}
+
+  searchCoin() {
+    this.filteredCoins = this.coins.filter(
+      (coin) =>
+        coin.name.toLowerCase().includes(this.searchText.toLowerCase()) ||
+        coin.symbol.toLowerCase().includes(this.searchText.toLowerCase())
+    );
+  }
 
   // ngOnInit(): similar a useEffect(() => {}, [])
   // .subscribe(): similar a fetch()
   //TODO: update code (subscribe)
   ngOnInit() {
-    this.http
+    /* this.http
       .get<Coin[]>(
         'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en'
       )
       .subscribe(
         (res) => {
           this.coins = res;
+          this.filteredCoins = res;
         },
         (err) => console.log(err)
-      );
+      ); */
   }
 }
