914329198a262b589c8d710d385e214e6936eefe
ariel <ariel.wm1@gmail.com>
2023-05-05 11:19:19 -0400

    config api coingecko

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+
+interface Coin {
+  id: string;
+  name: string;
+  symbol: string;
+  image: string;
+  current_price: number;
+  price_change_percentage_24: number;
+  total_volume: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  coins: Coin[] = [];
+
   constructor(private http: HttpClient) {}
+
+  // ngOnInit(): similar a useEffect(() => {}, [])
+  // .subscribe(): similar a fetch()
+  //TODO: update code (subscribe)
+  ngOnInit() {
+    this.http
+      .get<Coin[]>(
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en'
+      )
+      .subscribe(
+        (res) => {
+          this.coins = res;
+        },
+        (err) => console.log(err)
+      );
+  }
 }
